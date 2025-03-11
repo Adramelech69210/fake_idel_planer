18988415825d349e3959952dffd994b75aa11309
Acetone <antoine.c@etik.com>
2025-03-11 14:52:53 +0100

    add appointments in seeds and correct report necessit

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_112501) do
     t.bigint "user_id", null: false
     t.bigint "patient_id", null: false
     t.text "summary"
-    t.bigint "report_id", null: false
+    t.bigint "report_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
@@ -118,7 +118,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_112501) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "appointments", "patients"
-  add_foreign_key "appointments", "reports"
   add_foreign_key "appointments", "users"
   add_foreign_key "notes", "patients"
   add_foreign_key "pathologies", "patients"
