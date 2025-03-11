c29cab8e3e47fc3b15494afcfdd7a9c3eedf8fcb
Nabil Ghachem <gach.nabil@gmail.com>
2025-03-11 11:08:25 +0100

    edited a validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ApplicationRecord
   belongs_to :patient
   belongs_to :report
 
-  validates :start_date, :end_date, :summary, presence: true
+  validates :start_date, :end_date, presence: true
   validates :start_date, comparison: { less_than: :end_date }
 end
