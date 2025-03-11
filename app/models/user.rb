281f8a4db8201de2de3f443e1aa2913ebf2016d0
Jeremy Ligaon <jeremy.ligaon@gmail.com>
2025-03-11 13:32:15 +0100

    fix belongs_to

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  belongs_to :group, optional: true
   has_one_attached :photo
 
   def profile_picture
