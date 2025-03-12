2bab7d5e090637f8da085730650659fc9e7a7c8a
Nabil Ghachem <gach.nabil@gmail.com>
2025-03-12 15:19:07 +0100

    optimization

--- app/controllers/pathologies_controller.rb
+++ app/controllers/pathologies_controller.rb
@@ -1,12 +1,17 @@
 class PathologiesController < ApplicationController
   before_action :set_patient
   before_action :set_pathology, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @pathologies = @patient.pathologies
   end
+
   def new
     @pathology = @patient.pathologies.new
   end
+
+
   def create
     @pathology = @patient.pathologies.new(pathology_params)
     if @pathology.save
@@ -15,30 +20,36 @@ class PathologiesController < ApplicationController
       render :new
     end
   end
+
   def show
   end
+
   def edit
   end
+
   def update
     if @pathology.update(pathology_params)
       redirect_to patient_pathologies_path(@patient), notice: 'Pathologie mise à jour avec succès.'
-    else
-      puts @pathology.errors.full_messages
+	@@ -35,22 +27,17 @@ def update
       render :edit
     end
   end
+
   def destroy
     @pathology.destroy!
     redirect_to patient_pathologies_path(@patient), notice: 'Pathologie supprimée avec succès.'
   end
+
   private
+
   def set_patient
     @patient = Patient.find(params[:patient_id])
   end
+
   def set_pathology
     @pathology = @patient.pathologies.find(params[:id])
   end
+
   def pathology_params
     params.require(:pathology).permit(:description)
   end
-end
