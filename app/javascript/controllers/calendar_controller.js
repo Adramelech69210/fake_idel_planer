2f8d0fdc9fea682f597b9390eb0e551db951e272
Acetone <antoine.c@etik.com>
2025-03-18 17:37:00 +0100

    some style edit

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -2,12 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="calendar"
 export default class extends Controller {
-  static targets = ["overlay"]
+  static targets = ["overlay", "overlayFilter"]
 
   open() {
     this.overlayTarget.classList.add("active")
+    this.overlayFilterTarget.classList.add("active")
+
   }
+
   close() {
+
     this.overlayTarget.classList.remove("active")
+    this.overlayFilterTarget.classList.add("hidden")
+
+    setTimeout(() => {
+      this.overlayFilterTarget.classList.remove("active", "hidden")
+    }, 200);    t
+
+
   }
 }
